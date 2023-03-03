f76ab5bb00d00d9faa37167309005f24a1be1d00
Justin Lee <lee.justin001126@gmail.com>
2023-03-03 09:11:29 -0800

    Add logic to past test for Order Constructor

---
+++ OrderTracker/Models/Order.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace OrderTracker.Models
+{
+  public class Order
+  {
+    public string Description { get; set; }
+    public string Quantity { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+
+    public Order(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    } 
+
+  }
+}
+
