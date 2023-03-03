d72f90f1024dff1e4f2e271880b3955cb9c2bc7f
Justin Lee <lee.justin001126@gmail.com>
2023-03-03 10:17:28 -0800

    add more functionaltiy to constructor that takes in Title etc. Updated tests

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -16,18 +16,18 @@ namespace OrderTracker.Tests
   [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("Test");
+      Order newOrder = new Order("Test", "placeholder", "placeholder", "placeholder");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
     [TestMethod]
     public void OrderConstructor_ReturnsOrderDescription_Stirng()
     {
-      string Description = "Order of Flour";
-      Order newOrder = new Order(Description);
-      string result = newOrder.Description;
+      string Title = "Order of Flour";
+      Order newOrder = new Order(Title, "placeholder", "placeholder", "placeholder");
+      string result = newOrder.Title;
 
-      Assert.AreEqual(Description, result);
+      Assert.AreEqual(Title, result);
     }
 
     [TestMethod]
@@ -45,8 +45,8 @@ namespace OrderTracker.Tests
     {
       string description01 = "Order of Flour From Old Factory";
       string description02 = "Order of Eggs from Scandanavia 2.0";
-      Order newOrder1 = new Order(description01);
-      Order newOrder2 = new Order(description02);
+      Order newOrder1 = new Order(description01, "placeholder", "placeholder", "placeholder");
+      Order newOrder2 = new Order(description02, "placeholder", "placeholder", "placeholder");
       List<Order> newList = new List<Order> { newOrder1, newOrder2 };
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -56,7 +56,7 @@ namespace OrderTracker.Tests
     public void GetId_OrdersAreHaveAnID_IDValue()
     {
       string description = "This is useless, I want an ID!";
-      Order newOrder = new Order(description);
+      Order newOrder = new Order(description, "placeholder", "placeholder", "placeholder");
       int result = newOrder.Id;
       Assert.AreEqual(1, result);
     }
@@ -66,8 +66,8 @@ namespace OrderTracker.Tests
     {
       string description01 = "This is the first order!";
       string description02 = "This is the second order!";
-      Order newOrder1 = new Order(description01);
-      Order newOrder2 = new Order(description02);
+      Order newOrder1 = new Order(description01, "placeholder", "placeholder", "placeholder");
+      Order newOrder2 = new Order(description02, "placeholder", "placeholder", "placeholder");
       Order result = Order.Find(2);
       Assert.AreEqual(newOrder2, result);
 
