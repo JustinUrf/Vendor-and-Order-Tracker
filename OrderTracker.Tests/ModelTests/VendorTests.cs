5bc80437a4e0d588b45f3634a1df478b7ff66f51
Justin Lee <lee.justin001126@gmail.com>
2023-03-03 10:53:45 -0800

    add basic boiler plate tests for Vendor Tests

---
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    [TestMethod
+  }
+}
